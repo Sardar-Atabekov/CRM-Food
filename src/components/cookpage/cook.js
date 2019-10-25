7ee205352271bdbda09b2d9b70fe9cb104522fb5
Sardar <sardarbekatabekov@gmail.com>
2019-10-26 05:01:39 +0600

    Update

--- src/components/cookpage/cook.js
+++ src/components/cookpage/cook.js
@@ -1,14 +1,86 @@
 import React, { Component } from 'react';
-import { ReactDOM } from "react-dom";
-import {Route, Switch} from "react-router-dom";
+import axios from 'axios';
+import './cook.css';
 
 
-class CookPage extends Component{
-    render() {
-        return (
-            <div>Hello</div>
-        );
+const API = 'https://neobiscrmfood.herokuapp.com/api/';
+const DEFAULT_QUERY = 'cook/getactiveorders';
+
+
+class CookPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      isLoading: false,
+      error: null,
+    };
+  }
+  
+  async componentDidMount() {
+    this.setState({ isLoading: true });
+    try {
+      const result = await axios.get(API + DEFAULT_QUERY);
+ 
+      this.setState({
+        data: result.data,
+        isLoading: false,
+        
+      });
+    } catch (error) {
+      this.setState({
+        error,
+        isLoading: false
+      });
     }
+  }
+  render() {
+    const { data, isLoading, error } = this.state;
+    if (error) {
+      return <p>{error.message}</p>;
+    }
+    if (isLoading) {
+      return <p>Loading ...</p>;
+    }
+
+ 
+        return (
+            <div className="wrapper">
+            {console.log(data)}
+            {      
+                
+                data.map(a=>
+                    <div className="item" key={a.id}>
+                        <header>
+                            <span className="tableNumber">{a.id}</span>
+                            <span className="orderTime">{
+                            new Date(a.dateTimeOrdered).getHours()+":"
+                            +new Date(a.dateTimeOrdered).getMinutes()
+                            }
+                            
+                            </span>
+                            <button className="statusOrder">Готово!</button>
+                        </header>
+                        <main>
+                            <div className="comments">
+                                {a.comment}
+                            </div>
+                            
+                            <ul key>
+                            {a.mealsList.map(meal=><li key={meal.mealId}>{`${meal.mealName} x ${meal.quantity}`}</li>)}
+                            </ul>
+                            
+                           
+                        </main>
+                    </div> 
+                )
+            }   
+            </div>
+          );
+        }
+    
+    
 }
+
 
 export default CookPage;
--- src/components/cookpage/script.js
+++ src/components/cookpage/script.js
@@ -61,6 +61,92 @@ const getResource = async(url)=> {
     .then((body)=> {
         console.log('waiter get order: ', body);
     });
+    
+    let formData={
+        orderId: 9,
+        mealId: 6
+      };
 
+    postData('https://neobiscrmfood.herokuapp.com/api/cook/closemeal', formData)
+    .then(data => console.log(JSON.stringify(data))) // JSON-строка полученная после вызова `response.json()`
+    .catch(error => console.error(error));
+
+    function postData(url, data) {
+  // Значения по умолчанию обозначены знаком *
+    return fetch(url, {
+        method: 'POST', // GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, cors, *same-origin
+        // cache: 'no-cache', // default, no-cache, reload, force-cache, only-if-cached
+        // credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json',
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        // redirect: 'follow', // manual, *follow, error
+        // referrer: 'no-referrer', // no-referrer, *client
+        body: JSON.stringify(data), // тип данных в body должен соответвовать значению заголовка "Content-Type"
+    })
+    .then(response => response.json()); // парсит JSON ответ в Javascript объект
+}
+
+    // const putMethod = {
+    //     method: 'POST', // Method itself
+    //     headers: {
+    //      'Content-type': 'application/json; charset=UTF-8' // Indicates the content 
+    //     },
+    //     body: JSON.stringify(formData) // We send data in JSON format
+    //    };
     
+    //    console.log(putMethod.body);
+    //    fetch('https://neobiscrmfood.herokuapp.com/api/cook/closemeal', putMethod)
+    //    .then(data => console.log(data)) // Manipulate the data retrieved back, if we want to do something with it
+    //    .catch(err => console.log(err))// Do something with the erro
+    // (async () => {
+    //     const rawResponse = await fetch('https://neobiscrmfood.herokuapp.com/api/cook/closemeal', {
+    //       method: 'POST',
+    //       headers: {
+    //         'Accept': 'application/json',
+    //         'Content-Type': 'application/json',
+    //         "Access-Control-Allow-Origin" : "*", 
+    //         "Access-Control-Allow-Credentials" : true 
+    //       },
+    //       body: JSON.stringify({orderId: 9, mealId: 6})
+    //     });
+    //     const content = await rawResponse.json();
+      
+    //     console.log(content);
+    //   })();
+    // var payload = {
+    //     orderId: 9, 
+    //     mealId: 6
+    // }
     
+    // var data = new FormData();
+    // data.append( "json", JSON.stringify( payload ) );
+    
+    // fetch("https://neobiscrmfood.herokuapp.com/api/cook/closemeal",
+    // {
+    //     method: "POST",
+    //     body: data
+    // })
+    // .then(function(res){ return res.json(); })
+    // .then(function(data){ console.log( JSON.stringify( data ) ) })
+
+
+    // function createNewProfile(profile) {
+    //     const formData = new FormData();
+    //     formData.append('orderId', profile.orderId);
+    //     formData.append('mealId', profile.mealId);
+        
+    
+    //     return fetch('https://neobiscrmfood.herokuapp.com/api/cook/closemeal', {
+    //         method: 'POST',
+    //         body: formData
+    //     }).then(response => response.json())
+    // }
+    
+    // createNewProfile(a)
+    //    .then((json) => {
+    //        console.log(json);
+    //     })
+    //    .catch(error => error);
