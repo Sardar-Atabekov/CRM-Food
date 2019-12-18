6a51151fd67fc4f69bc8803a72eb1f037871b6a4
Sardar Atabekov <sardarbekatabekov@gmail.com>
2019-12-18 19:08:29 +0600

    done graphics in orders page

--- src/components/adminpage/statistics/orders/order.js
+++ src/components/adminpage/statistics/orders/order.js
@@ -6,11 +6,35 @@ import Total from "./totals";
 import NamePage from "../../blocks/namePage";
 import TopMeals from "./TopMeals";
 import TopWaiter from "./topWaiter";
-
+import { API, getData } from "./../../../requests";
+import Loading from "../../../loading/loading.js";
+import GraphicArt from "./../graphics/graphics";
 class Sales extends Component {
-  
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      graphics: false,
+      isLoading: false
+    };
+  }
 
+  async componentDidMount() {
+    getData(`${API}/Admin/waiterOrderTop`).then(body => {
+      this.setState({ data: body });
+    });
+  }
+  render() {
+    let { data } = this.state;
+
+    let sum, names;
+    if (data) {
+      data = data.sort((a, b) => b.orderCount - a.orderCount);
+      sum = [
+        ...data.map((item, index) => (index < 8 ? item.orderCount : false))
+      ];
+      names = [...data.map((item, index) => (index < 8 ? item.name : false))];
+    }
     return (
       <div className="wrapper">
         <aside className="navBlock">
@@ -20,15 +44,20 @@ class Sales extends Component {
           <header className="main-search">
             <Search />
           </header>
-          <main className="orderContent">
-            <NamePage name="Обзор заказов" />
-            <Total />
-            <div className="statistics">
-              <div className="graphicArt">Графика</div>
-              <TopMeals name="Топ блюд" />
-              <TopWaiter name="Топ официанты" />
-            </div>
-          </main>
+          {this.state.isLoading ? (
+            <main className="orderContent">
+              <NamePage name="Обзор заказов" />
+              <Total />
+              <div className="statistics">
+                <GraphicArt data={sum} names={names} name="Топ официанты" />
+                <TopMeals name="Топ блюд" />
+                <TopWaiter name="Топ официанты" />
+              </div>
+            </main>
+          ) : (
+            <Loading />
+          )}
+
           <footer className="main-footer">
             <Footer />
           </footer>
