28fee3350d62f043dbef8e618787dbabd1211341
Sardar Atavekov <sardarbekatabekov@gmail.com>
2020-02-01 17:25:27 +0600

    done pagination in history transaction page

--- src/components/adminpage/transaction/HistoryTransaction.js
+++ src/components/adminpage/transaction/HistoryTransaction.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { getData, API } from "../../requests";
 import Navigation from "../../block/navigation.js";
 import Search from "../../block/search.js";
@@ -9,19 +9,25 @@ import { TimeDate } from "./../calendar/time";
 import Loading from "../../loading/loading";
 import MoreModal from "./../../modalWindow/moreInfoModal";
 import "./transaction.css";
-class HistoryTransaction extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [],
-      isLoading: true,
-      moreModal: false,
-      id: "",
-      meals: ""
-    };
-  }
 
-  checkStatus(status) {
+const HistoryTransaction = () => {
+  const [page, setPage] = useState(1);
+  const [data, setData] = useState([]);
+  const [count, setCount] = useState(10);
+  const [loading, setLoading] = useState(true);
+  const [modalStatus, setModalStatus] = useState(false);
+  const [modalOrder, setModalOrder] = useState({});
+  useEffect(() => {
+    document.title = "Истории транзакции";
+    getData(
+      `${API}/Statistic/transactionHistory?&pageNumber=${page}&pageSize=${count}`
+    ).then(data => {
+      setLoading(false);
+      setData(data);
+    });
+  }, [count, page]);
+
+  const checkStatus = status => {
     let res =
       status === 0
         ? "Активный"
@@ -31,88 +37,100 @@ class HistoryTransaction extends Component {
         ? "Кухня готова"
         : "Бар готова";
     return res;
-  }
-  async componentDidMount() {
-    getData(`${API}/Statistic/transactionHistory`).then(body => {
-      this.setState({ data: body, isLoading: false });
-    });
-  }
+  };
 
-  render() {
-    let { data } = this.state;
-    console.log(data);
-    return (
-      <div className="wrapper">
-        <aside className="navBlock">
-          <Navigation />
-        </aside>
-        <div className="container">
-          <header className="main-search">
-            <Search />
-          </header>
-          {this.state.isLoading ? (
-            <Loading />
-          ) : (
-            <main className="waiterContent transaction">
-              <div className="functionPage">
-                <NamePage name="История транзакции" />
-                <Calendar />
-              </div>
-              {/* <div className="transactionDisplay">
-                <label htmlFor="show">Show</label>
-                <select id="show">
-                  <option value="10">10</option>
-                  <option value="25">25</option>
-                  <option value="50">50</option>
-                  <option value="100">100</option>
-                </select>
-                <label htmlFor="show">entries</label>
-              </div> */}
-              <table>
-                <tbody>
-                  <tr>
-                    <th className="sortingNumber">#</th>
-                    <th>Время</th>
-                    <th>Официант</th>
-                    <th>Блюда</th>
-                    <th>Статус</th>
-                    <th>Итого</th>
-                  </tr>
-                  {data.map(order => (
-                    <tr key={order.id}>
-                      <td>{order.id}</td>
-                      <td>
-                        <time dateTime={order.orderDate}>
-                          {TimeDate(order.orderDate)}
-                        </time>
-                      </td>
+  // const createPage = () => {
+  //   let buttons = [],
+  //     pages = Math.ceil(result.total / count);
+  //   for (let i = 0; i < pages; i++) {
+  //     buttons.push(
+  //       <Button
+  //         key={i}
+  //         className={
+  //           i === page
+  //             ? "paginationActiveButton shadow all-lessons-pagination all-lessons-pagination-inactive rounded-0 mr-3"
+  //             : "shadow all-lessons-pagination all-lessons-pagination-active rounded-0 mr-3 bg-white"
+  //         }
+  //         color={"faded"}
+  //         onClick={() => setPage(i)}
+  //       >
+  //         {i + 1}
+  //       </Button>
+  //     );
+  //   }
+  //   return buttons;
+  console.log(data);
+  return (
+    <div className="wrapper">
+      <aside className="navBlock">
+        <Navigation />
+      </aside>
+      <div className="container">
+        <header className="main-search">
+          <Search />
+        </header>
+        {loading ? (
+          <Loading />
+        ) : (
+          <main className="waiterContent transaction">
+            <div className="functionPage">
+              <NamePage name="История транзакции" />
+              <Calendar />
+            </div>
+            <div className="transactionDisplay">
+              <label htmlFor="show">Показать по</label>
+              <select id="show" onChange={e => setCount(e.target.value)}>
+                <option value="10">10</option>
+                <option value="25">25</option>
+                <option value="50">50</option>
+                <option value="100">100</option>
+              </select>
+            </div>
+            <table>
+              <tbody>
+                <tr>
+                  <th className="sortingNumber">#</th>
+                  <th>Время</th>
+                  <th>Официант</th>
+                  <th>Блюда</th>
+                  <th>Статус</th>
+                  <th>Итого</th>
+                </tr>
+                {data.map(order => (
+                  <tr key={order.id}>
+                    <td>{order.id}</td>
+                    <td>
+                      <time dateTime={order.orderDate}>
+                        {TimeDate(order.orderDate)}
+                      </time>
+                    </td>
 
-                      <td>{order.waiterName}</td>
-                      <td>
-                        {this.state.moreModal ? (
-                          <MoreModal
-                            id={this.state.id}
-                            meals={this.state.meals}
-                            setStatus={() =>
-                              this.setState({ moreModal: false })
-                            }
-                          />
-                        ) : (
-                          <span
-                            onClick={() => {
-                              this.setState({ moreModal: true });
-                              this.setState({ id: order.id });
-                              this.setState({ meals: order.mealOrders });
-                            }}
-                            className="moreMeals"
-                          >
-                            Посмотреть
-                          </span>
-                        )}
-                      </td>
-                      <td>{this.checkStatus(order.status)}</td>
-                      <td>{order.totalPrice} сом</td>
-                      {/* <td className="operationBlock">
+                    <td>{order.waiterName}</td>
+                    <td>
+                      {modalStatus ? (
+                        <MoreModal
+                          id={modalOrder.id}
+                          meals={modalOrder.meals}
+                          setStatus={() => setModalStatus(false)}
+                        />
+                      ) : (
+                        <span
+                          onClick={() => {
+                            setModalStatus(true);
+                            setModalOrder({
+                              id: order.id,
+                              meals: order.mealOrders
+                            });
+                          }}
+                          className="moreMeals"
+                        >
+                          Посмотреть
+                        </span>
+                      )}
+                    </td>
+                    <td>{checkStatus(order.status)}</td>
+                    <td>{order.totalPrice} сом</td>
+                    {/* <td className="operationBlock">
                        <div className="operation">
                         <Link to={{ pathname: `/order/${order.id}/` }}>
                           Изменить{" "}
@@ -127,20 +145,48 @@ class HistoryTransaction extends Component {
                         </button>
                       </div>
                     </td> */}
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </main>
-          )}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </main>
+        )}
 
-          <footer className="main-footer">
-            <Footer />
-          </footer>
-        </div>
+        <footer className="main-footer">
+          <Footer />
+        </footer>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default HistoryTransaction;
+
+// class HistoryTransaction extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       data: [],
+//       isLoading: true,
+//       moreModal: false,
+//       id: "",
+//       meals: ""
+//     };
+//   }
+
+//   async componentDidMount() {
+//     getData(`${API}/Statistic/transactionHistory`).then(body => {
+//       this.setState({ data: body, isLoading: false });
+//     });
+//   }
+
+//   render() {
+//     let { data } = this.state;
+//     console.log(data);
+//     return (
+
+//     );
+//   }
+// }
+
+// export default HistoryTransaction;
